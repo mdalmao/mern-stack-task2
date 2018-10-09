f3e0ffbd051f770c96c8329a5512be3b85193816
mdalmao <mdalmaouy@gmail.com>
2018-10-09 15:55:50 -0300

    Ajusto la Base de datos

--- src/database.js
+++ src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const URI = "mongodb+srv://prueba:VdaOIx6tqcgTBpOz@cluster0-sgraz.mongodb.net/test";
+//const URI = "mongodb+srv://prueba:VdaOIx6tqcgTBpOz@cluster0-sgraz.mongodb.net/test";
+const URI='mongodb://prueba:Sistemas1@localhost:27017/mern-tasks';
+
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
