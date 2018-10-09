3574e745a6cc7b9dd297926b295b803754210aa7
mdalmao <mdalmaouy@gmail.com>
2018-10-09 10:38:21 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const mongoose= require('mongoose');
 const URI='mongodb://localhost/mern-tasks'; // crea  la base si no existe
 
 mongoose.connect(URI)
-.then(db => console.log('DB esta conectada'))
+.then(db => console.log('DB esta conectada2'))
 .catch(err=>console.error(err));
 
 module.exports = mongoose;
