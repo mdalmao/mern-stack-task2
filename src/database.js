39bcf1a1802404b00d8cf505fe26570a0953abfb
mdalmao <mdalmaouy@gmail.com>
2018-10-09 16:15:46 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose');
 
-//const URI = "mongodb+srv://prueba:VdaOIx6tqcgTBpOz@cluster0-sgraz.mongodb.net/test";
-const URI='mongodb://prueba:Sistemas1@172.30.243.232:27017/mern-tasks';
+const URI = "mongodb+srv://prueba:VdaOIx6tqcgTBpOz@cluster0-sgraz.mongodb.net/test";
+//const URI='mongodb://prueba:Sistemas1@172.30.243.232:27017/mern-tasks';
 
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
     .catch(err => console.error(err));
 
 module.exports = mongoose;
-
-
-
