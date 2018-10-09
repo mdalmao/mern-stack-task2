df45f47eb8730c67a910161f4b631981f848866b
mdalmao <mdalmaouy@gmail.com>
2018-10-09 11:35:06 -0300

    Actualizo  Base
    Actualizo la base

--- src/database.js
+++ src/database.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 
-//const URI='mongodb://localhost/mern-tasks'; // crea  la base si no existe
 const URI = "mongodb+srv://prueba:VdaOIx6tqcgTBpOz@cluster0-sgraz.mongodb.net/test";
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
