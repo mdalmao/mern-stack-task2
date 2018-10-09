3fe727ac46c38c6d4569391eb34fe25eb1f83288
mdalmao <mdalmaouy@gmail.com>
2018-10-09 16:08:37 -0300

    Update database.js

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 //const URI = "mongodb+srv://prueba:VdaOIx6tqcgTBpOz@cluster0-sgraz.mongodb.net/test";
-const URI='mongodb://prueba:Sistemas1@localhost:27017/mern-tasks';
+const URI='mongodb://prueba:Sistemas1@172.30.243.232:27017/mern-tasks';
 
 mongoose.connect(URI, { useNewUrlParser: true })
     .then(db => console.log('DB is connected'))
